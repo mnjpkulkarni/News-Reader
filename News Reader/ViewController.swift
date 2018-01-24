3d34d81fa096739742416c8f32f5fa7de4fe2be8
manojkulkarni <mnjpkulkarni@gmail.com>
2018-01-24 16:40:40 -0500

    Initial Commit

---
+++ News Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News Reader
+//
+//  Created by Manoj Kulkarni on 1/24/18.
+//  Copyright © 2018 Manoj Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ News ReaderTests/News_ReaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  News_ReaderTests.swift
+//  News ReaderTests
+//
+//  Created by Manoj Kulkarni on 1/24/18.
+//  Copyright © 2018 Manoj Kulkarni. All rights reserved.
+//
+
+import XCTest
+@testable import News_Reader
+
+class News_ReaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
